a754e4b6cf84767d314316785375d1a33e69f22e
Quoc Hoang <hoangvanquoc923@gmail.com>
2025-06-25 15:15:57 +0700

    fix: adjust the Subject Repo type

--- src/main/java/com/gotest/gscore/repository/SubjectRepo.java
+++ src/main/java/com/gotest/gscore/repository/SubjectRepo.java
@@ -1,12 +1,12 @@
 package com.gotest.gscore.repository;
 
 import com.gotest.gscore.entity.Student;
+import com.gotest.gscore.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface SubjectRepo extends JpaRepository<Student, String> {
-    Optional<Student> findBySbd(String sbd);
+public interface SubjectRepo extends JpaRepository<Subject, String> {
 }
