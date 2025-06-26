dc1a64ec7b28ef0c855a83ca90adb87ef7248dfb
Quoc Hoang <hoangvanquoc923@gmail.com>
2025-06-26 16:17:03 +0700

    chore: add cors config to allow external request

---
+++ src/main/java/com/gotest/gscore/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.gotest.gscore.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false) // Set to true if credentials are needed
+                .maxAge(3600); // Cache preflight response for 1 hour
+    }
+}
