22210579318da2e141596d5d618f21b6fd5e67e6
Hoang Van Quoc <94336051+HVQuoc@users.noreply.github.com>
2025-06-26 16:27:14 +0700

    fix: Update CorsConfig.java (annotation :>)

--- src/main/java/com/gotest/gscore/config/CorsConfig.java
+++ src/main/java/com/gotest/gscore/config/CorsConfig.java
@@ -3,6 +3,7 @@ package com.gotest.gscore.config;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
